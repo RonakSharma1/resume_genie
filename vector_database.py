a1b4448cac4b09da3badc7958af1ba37348c0874
Ronak Sharma <rsharma29230@gmail.com>
2025-01-01 17:11:27 +0000

    use uuid as index in db

--- vector_database.py
+++ vector_database.py
@@ -1,7 +1,10 @@
-from config import NUMBER_OF_RESULTS_TO_RETURN, VECTOR_COLLECTION_NAME
+from parameters import NUMBER_OF_RESULTS_TO_RETURN, VECTOR_COLLECTION_NAME
 import embeddings
+import uuid
+from pandas import DataFrame
+from typing import Any
 
-def store_document_in_vector_db(chroma_client,resume_latest_data_df):
+def store_document_in_vector_db(chroma_client: Any,resume_latest_data_df: DataFrame):
     collection_of_resumes_db = chroma_client.get_or_create_collection(name=VECTOR_COLLECTION_NAME)
     
     # use lambda instead of dataframe iteration
@@ -9,12 +12,12 @@ def store_document_in_vector_db(chroma_client,resume_latest_data_df):
         collection_of_resumes_db.add(
             embeddings = [row["embeddings"]],
             documents = [row["Resume_str"]],
-            ids = [str(row["index"])],
+            ids = [str(uuid.uuid4())],
             metadatas = [{"doc_id":str(row["ID"])}]
         )
 
 
-def query_vector_db_for_user_question(openai_client, chroma_client, question):
+def query_vector_db_for_user_question(openai_client: Any, chroma_client: Any, question: str):
     # Fetch the vector db collection
     collection_of_resumes_db = chroma_client.get_or_create_collection(name=VECTOR_COLLECTION_NAME)
 
@@ -29,7 +32,7 @@ def query_vector_db_for_user_question(openai_client, chroma_client, question):
     return _extract_optimal_solution(results)
     
 
-def _extract_optimal_solution(vector_db_search_result):
+def _extract_optimal_solution(vector_db_search_result: Any):
     # extracting the top matching Resume and its ID
     best_result_document = vector_db_search_result.get('documents')[0][0]
     best_result_doc_id = int(vector_db_search_result.get('metadatas')[0][0].get("doc_id"))
